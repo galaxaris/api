a318d0becbc8d590bf5c7b851a923ef955c0c7a3
Raphix <picot.raphael@gmail.com>
2026-02-28 20:22:48 +0100

    feat: add Image and edit things

--- UI/Menu.py
+++
@@ -1,9 +0,0 @@
-class Menu:
-    def __init__(self, name, items):
-        self.name = name
-        self.items = items
-
-    def display(self):
-        print(f"Menu: {self.name}")
-        for item in self.items:
-            print(f"- {item}")
