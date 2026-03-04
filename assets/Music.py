e24472062594da5296d5603d8fb973214f82f3a0
GauGoth-Corp <ggaugoth@gmail.com>
2026-03-04 22:49:28 +0100

    Refactor: Music & SFX

--- assets/Music.py
+++ assets/Music.py
@@ -1,18 +1,28 @@
 import pygame as pg
 
 class Music:
+    """
+    Class for handling music. 
+    
+    NOTE: Different from Sfx, because only one music can be played at a time, on a reserved channel.
+
+    This allows to handle music easier, without worrying about channels or overlapping sounds 
+    ==> if a new music is played, the previous one will automatically stop.
+
+    If wanted, a music can still be played as a Sfx.
+    """
     def __init__(self, path: str):
         self.path = path
-        self.sound = pg.mixer.Sound(path)
+        self.sound = pg.mixer.music.load(path)
 
     def play(self, loops: int = 0):
-        self.sound.play(loops=loops)
+        pg.mixer.music.play(loops=loops)
 
     def stop(self):
-        self.sound.stop()
+        pg.mixer.music.stop()
 
     def set_volume(self, volume: float):
-        self.sound.set_volume(volume)
+        pg.mixer.music.set_volume(volume)
 
     def get_volume(self) -> float:
-        return self.sound.get_volume()
+        return pg.mixer.music.get_volume()
