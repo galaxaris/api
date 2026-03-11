f0f6ebb38a4910e6a2c6f7b44110eb8f322f41c4
Axelotl-30 <axelgirad7@laposte.net>
2026-03-11 23:08:33 +0100

    projectiles inital pos is now centered on the projectile so they follow the trajectory drawn a bit better

--- entity/Projectile.py
+++ entity/Projectile.py
@@ -5,9 +5,9 @@ from api.entity.Entity import Entity
 
 
 class Projectile(Entity):
-    def __init__(self, pos: tuple[int, int] | pg.Vector2, gravity: float, shot_speed: float, angle_radians: float, size: tuple[int, int] = (8,8), effect: str = None, target: str = None, Time = None):
+    def __init__(self, pos: tuple[int, int] | pg.Vector2, gravity: float, shot_speed: float, angle_radians: float, size: tuple[int, int] | pg.Vector2 = (8,8), effect: str = None, target: str = None, Time = None):
         super().__init__(pos, size)
-        self.pos = pg.Vector2(pos)
+        self.pos = pg.Vector2(pos) - self.size/2
         self.vel = shot_speed * pg.Vector2(math.cos(angle_radians), -math.sin(angle_radians))
         self.add_tag("projectile")
         self.image = pg.Surface((8, 8))
