8d564faac72440eb73ab123e2bf4968bfa654a33
Raphix <picot.raphael@gmail.com>
2026-03-12 16:33:15 +0100

    fix: Inventory issue

--- items/Inventory.py
+++ items/Inventory.py
@@ -2,7 +2,7 @@ from api.UI.GameUI import UIElement, GameUI
 
 
 class Inventory(UIElement):
-    def __init__(self, pos, size):
+    def __init__(self, pos=(0,0), size=(0,0)):
         super().__init__(pos, size)
         self.items = []
         self.UI = GameUI(size)
