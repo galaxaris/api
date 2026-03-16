25c3be558d9646dccfd28a55a4967dd0c30996a8
Raphix <picot.raphael@gmail.com>
2026-03-16 10:28:51 +0100

    feat: add stage to boss behavior

--- entity/Boss.py
+++ entity/Boss.py
@@ -13,9 +13,18 @@ class Boss(Enemy):
         self.name = name
         self.enemy_progress_bar = EntityProgressBar((100, 100, 100), (255, 0, 0), progress_max=health, name=self.name)
         self.add_tag("boss")
+        self.stages: dict[str, callable] = []
+        self.stages_order: dict[str, int] = []
 
     def update(self, scene=None):
         Character.update(self, scene)
+
+
+        self.stages_order = dict(sorted(self.stages_order.items(), key=lambda item: item[1], reverse=True))
+        for stage_name, behavior in self.stages:
+            if self.health <= self.stages_order[stage_name]:
+                behavior(self, scene)
+                break
 
         if self.enemy_progress_bar:
             if self.health > 0:
@@ -39,3 +48,6 @@ class Boss(Enemy):
     def set_behavior(self, behavior: callable):
         self.behavior = behavior
 
+    def add_stage(self, name: str, behavior: callable, life_threshold: int):
+        self.stages.append((name, behavior))
+        self.stages_order[name] = life_threshold
